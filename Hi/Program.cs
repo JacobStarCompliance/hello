25a383fed6bf99f33813e717d5d26aad220bc088
Jacob Busfield <jacob.busfield@starcompliance.com>
2017-07-11 14:50:19 +0100

    Random commit!

---
+++ Hi/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var something = Console.ReadKey(true).KeyChar;
+        }
+    }
+}
